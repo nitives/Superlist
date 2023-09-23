dbc2afc514918d378e1af27f4c867308f6407dc0
nitives <85454682+nitives@users.noreply.github.com>
2023-09-23 08:50:47 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-const body = document.body;
-
-darkModeToggle.addEventListener("click", () => {
-    body.classList.toggle("dark-mode");
-});
