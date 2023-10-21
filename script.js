9cdba5698ab5c03d4c75dae8ea3a06c14bb17e25
nitives <cadenrichards15@gmail.com>
2023-10-21 00:18:20 -0400

    AD DETECT!

--- script.js
+++ script.js
@@ -36,3 +36,20 @@ toggleDarkMode();
 
 // Add click event listener to the dark mode toggle button
 darkModeToggle.addEventListener('click', toggleDarkMode);
+
+// Ad Black Detect
+    const detect = document.querySelector("#detect");
+    const wrapper = document.querySelector(".wrapper");
+    const button = wrapper.querySelector("button");
+
+    let adClasses = ["ad", "ads", "adsbox", "doubleclick", "ad-placement", "ad-placeholder", "adbadge", "BannerAd"];
+    for(let item of adClasses){
+      detect.classList.add(item);
+    }
+    let getProperty = window.getComputedStyle(detect).getPropertyValue("display");
+    if(!wrapper.classList.contains("show")){
+      getProperty == "none" ? wrapper.classList.add("show") : wrapper.classList.remove("show");
+    }
+    button.addEventListener("click", ()=>{
+      wrapper.classList.remove("show");
+    });
