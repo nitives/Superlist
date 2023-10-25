983a1af6088ac96d9ef552216797af6c44a4b644
nitives <cadenrichards15@gmail.com>
2023-10-25 01:49:01 -0400

    Huge

--- script.js
+++ script.js
@@ -85,3 +85,62 @@ window.addEventListener("load", removeControls);
 window.addEventListener("resize", removeControls);
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------
+
+// Enable JSHint for ES6
+/*jshint esversion: 6 */
+
+// On document ready, execute the initial setup
+$(document).ready(function () {
+    checkForQuickLink();
+});
+
+// Check for the presence of a QuickLink parameter in the URL
+function checkForQuickLink() {
+    const url = new URL(window.location.href);
+    const ql = url.searchParams.get("ql");
+
+    if (ql === "1") {
+        const decodedUrl = decodeURIComponent(url.searchParams.get("url"));
+        $("#url").val(decodedUrl);
+        $("#download_btn").trigger('click');
+    }
+}
+
+// Handle Download Button click event
+$("#download_btn").on("click", function () {
+    const url = $("#url").val();
+    const myAdUrl = "https://ads.com";
+    const iframeHTML = `
+        <iframe class="mx-auto mb-3" style="width:800px;height:250px;border:0;overflow:hidden;" 
+                scrolling="no" 
+                src="https://loader.to/api/card/?url=${url}&adUrl=${myAdUrl}">
+        </iframe>`;
+
+    $("#download_row").prepend(iframeHTML);
+});
+
+// Update QuickLink URL when input value changes
+$("#qlurl").on('input', function () {
+    const url = encodeURIComponent($(this).val());
+    const ql = `https://DOWNLOADER.com/?ql=1&url=${url}`;
+    $("#quicklink").val(ql);
+});
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------
+
+function downloadVideo() {
+    // Get the user input from the input field
+    var userInput = document.getElementById("urlInput").value;
+
+    if (userInput) {
+        // Construct the download link
+        var downloadLink = "https://loader.to/api/button/?url=" + userInput;
+
+        console.log("Download link: " + downloadLink);
+
+        // Redirect to the download link
+        window.location.href = downloadLink;
+    } else {
+        console.log("Error: No URL provided");
+    }
+}
