bc1b67ded3dcb6a1749668a5707bac40038438f5
n <85454682+nitives@users.noreply.github.com>
2024-01-24 18:54:10 -0500

    TATA

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './dist/*.{html,js}',
-  ],
+  content: ['./dist/*.{html,js}'],
   theme: {
     extend: {
       spacing: {
